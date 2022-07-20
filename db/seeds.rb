c413e92897309f914fefda61f6faf70a898401d9
Eduarda Campos <eduarda.prc@gmail.com>
2022-07-20 13:43:29 -0300

    Change in frontend and added heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+# Get a list of 20 top movies
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+url_open = URI.open(url).read
+top_movies = JSON.parse(url_open)
+
+top_movies['results'].each do |result|
+  puts("Creating #{result['title']}")
+  Movie.create(title: result['title'],
+                overview: result['overview'],
+                poster_url: "https://image.tmdb.org/t/p/original#{result['poster_path']}",
+                rating: result['vote_average'])
+end
+puts("Ended top movies creation!")
+
+# Get a list of 20 popular movies
+url = 'https://tmdb.lewagon.com/movie/popular'
+url_open = URI.open(url).read
+top_movies = JSON.parse(url_open)
+
+top_movies['results'].each do |result|
+  puts("Creating #{result['title']}")
+  Movie.create(title: result['title'],
+                overview: result['overview'],
+                poster_url: "https://image.tmdb.org/t/p/original#{result['poster_path']}",
+                rating: result['vote_average'])
+end
+puts("Ended top movies creation!")
